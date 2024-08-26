3415053789c98f36745c589a9fb398bcec29d8f5
SinchanaAJ <sinchanacharya23@gmail.com>
2024-08-27 01:23:30 +0530

    Initial commit to new repository

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -56,6 +56,6 @@ app.post('/api/transactions', async (req, res) => {
   
   
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
